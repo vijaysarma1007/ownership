d73b1b0daf2547b806a308e34c0b7761cd337ddc
vijaysarma1007 <svmsarma53@gmail.com>
2026-02-22 15:23:58 +0530

    added owndership

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let food: &str = "pasta";
+
+    let text = String::new();
+    let candy = String::from("KitKat");
+
+    let mut name = String::from("Boris");
+    println!("{name}");
+    name.push_str(" Pask");
+
+    println!("{name}");
+}
